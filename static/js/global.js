de6acc954875b3f4dbca03cccd500deb1f8932f6
Ryan Poe <baublet@gmail.com>
2018-08-14 12:57:18 -0500

    Add photo gallery templates

--- static/js/global.js
+++ static/js/global.js
@@ -1,22 +1,38 @@
+// Tools
+
+// forEach method to loop through anything
+var forEach = function(array, callback, scope) {
+	for (var i = 0; i < array.length; i++) {
+		callback.call(scope, array[i], i);
+	}
+};
+
 // Sidebar swiping
+var sidebar = document.querySelector(".sidebar");
 
-var sidebar = document.querySelector(".sidebar")
+if (sidebar) {
+	var hammerSidebar = new Hammer(sidebar);
 
-if(sidebar) {
-	var hammerSidebar = new Hammer(sidebar)
-	
 	hammerSidebar.on("swiperight", function(ev) {
-		if(!sidebar.classList) return
-		sidebar.classList.remove("shown")
-	})
+		if (!sidebar.classList) return;
+		sidebar.classList.remove("shown");
+	});
 
 	hammerSidebar.on("swipeleft", function(ev) {
-		if(!sidebar.classList) return
-		sidebar.classList.add("shown")
-	})
+		if (!sidebar.classList) return;
+		sidebar.classList.add("shown");
+	});
 
 	hammerSidebar.on("tap", function(ev) {
-		if(!sidebar.classList) return
-		sidebar.classList.add("shown")
-	})
+		if (!sidebar.classList) return;
+		sidebar.classList.add("shown");
+	});
+}
+
+// Initialize galleries if we have any
+var galleries = document.querySelectorAll(".photo-gallery");
+if (galleries && lightGallery) {
+	forEach(galleries, function(gallery) {
+		lightGallery(gallery);
+	});
 }
